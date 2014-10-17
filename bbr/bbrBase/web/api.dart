b93c10e11e46a03cce014d5838ce0f1b61e2a58c
Øystein Grændsen <oysteigr@gmail.com>
2014-10-17 17:43:40 +0200

    Added func for sending messages

--- bbr/bbrBase/web/api.dart
+++ bbr/bbrBase/web/api.dart
@@ -3,14 +3,15 @@ library api;
 import 'package:angular/angular.dart';
 
 class Api{
-  String SEND_MESSAGE_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/sendpifmelding';
-  String GET_REGISTERED_CODES_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentregistrertekoder/{lagId}';
-  String GET_PIF_POS_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentpifposisjon/{lagId}';
-  String GET_CURRENT_POST_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/{lagId}';
+  static final String SEND_MESSAGE_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/sendpifmelding';
+  static final String GET_REGISTERED_CODES_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentregistrertekoder/{lagId}';
+  static final String GET_PIF_POS_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentpifposisjon/{lagId}';
+  static final String GET_CURRENT_POST_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/{lagId}';
   
-  Http _http;
+  //Http _http;
   
   String getPifPos() {
+    Http _http;
     _http(method: 'GET', url: GET_PIF_POS_URL).
       then((HttpResponse res) {
         return res;
@@ -18,7 +19,18 @@ class Api{
     return 'temp';
   }
   
-  void sendMsg(){
+  static void sendHimmelretning(String retning){
+    Http _http;
+    _http.post(SEND_MESSAGE_URL, null);
+  }
+
+  static void sendDistanse(int dist){
+    Http _http;
+    _http.post(SEND_MESSAGE_URL, null);
+  }
+
+  static void sendStopp(bool stopp){
+    Http _http;
     _http.post(SEND_MESSAGE_URL, null);
   }
 }
