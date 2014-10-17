c678cf9c0083e7aa3498048123de194a4ca713b5
EdvardOr <edvard.oroug@bouvet.no>
2014-10-17 23:56:16 +0200

    .

--- bbr/bbrBase/web/api.dart
+++ bbr/bbrBase/web/api.dart
@@ -6,24 +6,21 @@ import 'dart:html';
 
 class Api{
   static final String SEND_MESSAGE_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/sendpifmelding';
-  static final String GET_REGISTERED_CODES_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentregistrertekoder/{lagId}';
-  static final String GET_PIF_POS_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentpifposisjon/{lagId}';
-  static final String GET_CURRENT_POST_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/{lagId}';
+  static final String GET_REGISTERED_CODES_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentregistrertekoder/{2cedfbe}';
+  static final String GET_PIF_POS_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentpifposisjon/{2cedfbe}';
+  static final String GET_CURRENT_POST_URL = 'http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/{2cedfbe}';
   static final String LAG_ID = "2cedfbe";
   
   //Http _http;
   
   String getPifPos() {
-    Http _http;
-    _http(method: 'GET', url: GET_PIF_POS_URL).
-      then((HttpResponse res) {
-        return res;
-      });
-    return 'temp';
+    String post = HttpRequest.getString(GET_PIF_POS_URL).toString();
+    return post;
   }
   
   static String getPost() {
-    return 'temp';
+    String post = HttpRequest.getString(GET_REGISTERED_CODES_URL).toString();
+    return post;
   }
   
   static void sendHimmelretning(String retning){
