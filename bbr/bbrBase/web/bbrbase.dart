978bb8700fa9a9a7b3d459eefb83e22463e8eb10
Øystein Grændsen <oysteigr@gmail.com>
2014-10-18 10:00:27 +0200

    ho

--- bbr/bbrBase/web/bbrbase.dart
+++ bbr/bbrBase/web/bbrbase.dart
@@ -65,8 +65,14 @@ void getPostBadge(Event e) {
   
  void onDataLoadedCodes(String responseText) {
   var jsonString = responseText;
+  List data = JSON.decode(responseText);
   InputElement input = querySelector('#outputPost');
-  querySelector('#codes').text = "testing";
+  querySelector('#codes').text = data[0];
+  querySelector('#list').children.clear;
+  querySelector('#list').appendHtml("<li>diiid</li>");
+  for(int i = 0; i < data.length; i++){
+    querySelector('#list').appendHtml("<li>" + data[i] + "</li>");
+  }
   input.value = jsonString;
 }
  
